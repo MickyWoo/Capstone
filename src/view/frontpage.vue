617fb300df890a7882d4a2ac5be206e93b58d4ac
Micky Woo <mwoo@seattleu.edu>
2020-08-09 19:18:30 -0700

    orign

--- src/view/frontpage.vue
+++ src/view/frontpage.vue
@@ -171,8 +171,7 @@ export default {
       axios
         .get(`http://newsapi.org/v2/top-headlines?country=us`, {
           params: {
-            'Access-Control-Allow-Origin': "newsapi.org",
-     
+            Origin: "http://newsapi.org/",
             pageSize: "5",
             category: "business",
             apiKey: "f702b0d64e0f48b5809e0c8db7c9a399",
