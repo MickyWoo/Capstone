1d336ed7f301fcc86d6925e9f9dbea74beaa76f3
Micky Woo <mwoo@seattleu.edu>
2020-08-26 13:32:05 -0700

    vue cli config

--- src/view/frontpage.vue
+++ src/view/frontpage.vue
@@ -267,10 +267,9 @@ export default {
   methods: {
     getNews: function () {
       axios
-        .get(`https://newsapi.org/v2/top-headlines?country=us`, {
+        .get(`http://localhost:8080/v2/top-headlines?country=us`, {
           params: {
-            crossorigin: "anonymous",
-       
+
             pageSize: "5",
             category: "business",
             apiKey: "f702b0d64e0f48b5809e0c8db7c9a399",
