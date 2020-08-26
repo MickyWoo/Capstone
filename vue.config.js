0a78d066b5b5d2c0dbdff9a1992c9ca11937bf2c
Micky Woo <mwoo@seattleu.edu>
2020-08-26 14:30:25 -0700

    comment out

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // https://stackoverflow.com/questions/54765638/vue-cli3-enable-cors
   configureWebpack: {
   devServer: {
-    proxy: 'https://newsapi.org',
+    // proxy: 'https://newsapi.org',
     headers: { "Access-Control-Allow-Origin": "*" }
 
   }
